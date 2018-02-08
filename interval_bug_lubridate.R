6347a9a3e59b3e0be3bad0cf9364aa25663621ed
Calum You <zedseayou@gmail.com>
2018-02-08 15:55:57 -0800

    add interval bug reprex

---
+++ interval_bug_lubridate.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(lubridate)
+
+tbl <- tibble(
+  x = 1:10,
+  start = seq(
+    from = ymd_hms("2018-01-01 12:00:00"),
+    to = ymd_hms("2018-02-01 12:00:00"),
+    length = 10
+    ),
+  end = seq(
+    from = ymd_hms("2018-01-01 15:00:00"),
+    to = ymd_hms("2018-02-01 15:00:00"),
+    length = 10
+  ),
+  interval = start %--% end
+)
+
+tbl %>% mutate(y = 1:10)
+tbl %>% select(everything())
+tbl %>% arrange(end)
+tbl %>% filter(x > 5)
+
+tbl %>% select(-interval)
+tbl %>% select(-interval) %>% mutate(y = 1:10)
+tbl %>% select(-interval) %>% select(everything())
+tbl %>% select(-interval) %>% arrange(end)
+tbl %>% select(-interval) %>% filter(x > 5)
+
+devtools::session_info()
+

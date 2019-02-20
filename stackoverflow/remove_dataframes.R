fbaad10d1e4affaeee5deadb1faebbaa7a84c8f6
Calum You <calum@uber.com>
2019-02-20 01:23:21 -0800

    first version of recursive function

---
+++ stackoverflow/remove_dataframes.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+x <- list(
+  p1 = list(type = "A", score = list(c1 = 10, c2 = 8, c3 = data.frame(a = 1, b = 3, c = 5))),
+  p2 = list(
+    type = "B", score = list(c1 = 9, c2 = 9, c3 = data.frame(a = 2, b = 2)),
+    p3 = list(type = "B", score = list(c1 = 9, c2 = 7, c3 = data.frame(a = 2, b = 2)))
+  )
+)
+
+remove_dataframes <- function(input_list) {
+  current_depth <- 1
+  current_list <- input_list
+  max_depth <- vec_depth(current_list)
+  while (current_depth < max_depth) {
+    browser()
+    current_list <- modify_depth(
+      .x = current_list,
+      .depth = current_depth,
+      .f = ~ discard(., is.data.frame)
+    )
+  current_depth <- current_depth + 1
+  max_depth <- vec_depth(current_list)
+  }
+  return(current_list)
+}
+
+x %>%
+  remove_dataframes %>%
+  view
+
+vec_depth(x)
+
+vec = 1:2
+modify(vec, ~ . + 1L)
+vec
